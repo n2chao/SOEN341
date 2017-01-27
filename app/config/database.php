c473425cff54d8487f8d52a67ea3301995bd464d
Ho Man Chan <homan98@gmail.com>
2017-01-27 14:56:53 -0500

    Database socket option in environment file
    Added environment variable DB_SOCK to configure with MAMP.
    
    *If you are using MAMP, your ‘.env’ file may need the following
    variable in your ‘.env’ file.
    
    MAC:
    DB_SOCK=/Applications/MAMP/tmp/mysql/mysql.sock

--- app/config/database.php
+++ app/config/database.php
@@ -51,7 +51,7 @@ return [
             'prefix' => '',
             'strict' => true,
             'engine' => null,
-            'unix_socket'   => '/Applications/MAMP/tmp/mysql/mysql.sock',
+            'unix_socket'   => env('DB_SOCK', ''),
         ],
 
         'pgsql' => [
