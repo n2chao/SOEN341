3d7e7771b6fb655b9d3bf0afe302030568cfb38a
Johnny Interiano <benjonant@msn.com>
2017-03-07 21:33:44 -0500

    #82 running the seeding command succesfully seeds tables for: users, courses, schedule, and enrollment

--- app/database/factories/ModelFactory.php
+++ app/database/factories/ModelFactory.php
@@ -22,3 +22,28 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Enrollment::class, function (Faker\Generator $faker) {
+
+    return [
+        'course_id' => $faker->numberBetween($min = 1, $max = 2),
+        'user_id' => $faker->unique()->numberBetween($min = 1, $max = 10),
+    ];
+});
+
+$factory->define(App\Schedule::class, function (Faker\Generator $faker) {
+
+    $userIds = App\User::pluck('id')->all();
+    $freetime = '';
+    for($i = 0; $i < 168; $i++){
+      $freetime = $freetime.$faker->numberBetween($min = 0, $max = 1);
+    }
+    return [
+        'freetime' => $freetime,
+        'user_id' => $faker->unique()->randomElement($userIds),
+    ];
+});
+
+
+
+?>
