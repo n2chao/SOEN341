73bed3e197e01ad986e65bf4dd07ead4685aa57c
Johnny Interiano <benjonant@msn.com>
2017-03-10 11:22:26 -0500

    #82, course_id column when seeded now displays the unique course codes

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
+
+use Illuminate\Http\Request;
 use Illuminate\Database\Seeder;
+use Illuminate\Validation\Rule;         //required for request validation
+use Illuminate\Support\Facades\Validator;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,24 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      
+
     factory(App\User::class, 10) -> create();
     factory(App\Schedule::class, 10) -> create();
 
-    DB::table('courses')->insert([
-        'code' => 'SOEN341',
-        'name' => 'SOEN341',
-    ]);
-    DB::table('courses')->insert([
-        'code' => 'ENGR371',
-        'name' => 'ENGR371',
-    ]);
-
-      factory(App\Enrollment::class, 10) -> create();
+      //factory(App\Enrollment::class, 10) -> create();
       //alternative once restriction on duplicate (course_id, user_id) gets created
 
-      /*$enrollments = factory(App\Enrollment::class, 10)->make();
+    factory(App\Enrollment::class, 10)->create();
+    
 
+
+/*
       foreach ($enrollments as $enrollment) {
           $repeat = true;
           while($repeat){
@@ -40,8 +38,8 @@ class DatabaseSeeder extends Seeder
                 $repeat = true;
             }
           }
-      }
-      */
+      }*/
+
 
       //when this code is refactored, it should reference other seeders
       //to keep each seeder file short. to be done using below command
