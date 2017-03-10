26b088820b2276210c8141c70231d7053ef79719
Ho Man Chan <homan98@gmail.com>
2017-03-10 12:08:01 -0500

    add installation middle ware #102

---
+++ app/app/Http/Middleware/Installation.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Auth;
+use App\Schedule;
+use App\Enrollment;
+class Installation
+{
+    /**
+     * Initial web installation
+     * Forces authenticated users to have courses and schedules
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+      $enrollments= Enrollment::where( 'user_id', Auth::id() )->oldest()->first();
+      if( !isset($enrollments) ) {
+        return redirect('courses');
+      }
+
+      $schedule = Schedule::where( 'user_id', Auth::id() )->oldest()->first();
+      if( !isset($schedule) ) {
+        return redirect('schedule/create');
+      }
+        return $next($request);
+    }
+}
