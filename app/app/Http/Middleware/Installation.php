51eef02a4fb88f2f9493955c82ab65e5c24a8acb
Ho Man Chan <homan98@gmail.com>
2017-03-13 22:43:20 -0400

    fix course enrollment #102

--- app/app/Http/Middleware/Installation.php
+++ app/app/Http/Middleware/Installation.php
@@ -20,7 +20,7 @@ class Installation
     {
       $enrollments= Enrollment::where( 'user_id', Auth::id() )->oldest()->first();
       if( !isset($enrollments) ) {
-        return redirect('course');
+        return redirect('courses/course');
       }
 
       $schedule = Schedule::where( 'user_id', Auth::id() )->oldest()->first();
