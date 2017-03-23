641cc9f7345f9f5968c74323414071f63667e410
Nathan Chao <n2chao@gmail.com>
2017-03-23 18:22:39 -0400

    Try another test with Travis for adding course that works locally

--- app/tests/Feature/EnrollmentTest.php
+++ app/tests/Feature/EnrollmentTest.php
@@ -6,6 +6,7 @@ use Tests\BrowserKitTestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Http\Request;
 use App\User;
 
 class EnrollmentTest extends BrowserKitTestCase
@@ -15,17 +16,20 @@ class EnrollmentTest extends BrowserKitTestCase
     use DatabaseTransactions;
 
     public function testAddCourseSuccess(){
-
+        /*
         factory(User::class)->create([
             'name' => 'george',
             'title' => 'student',
             'email' => 'george@george.com',
         ]);
         $user = \App\User::find(1);
+        $this->be($user);
+        */
+
+        $this->seed(\userSeed::class);  //creates a user with email bob@bob.com
+        $user = \App\User::find(1);
         $this->actingAs($user)
             ->visit('courses/course')
-            ->type('soen341', 'add_course_ids[0]')
-            ->type('soen341', 'add_course_ids[0]')
             ->type('soen341', 'add_course_ids[0]')
             ->press('Add Courses')
             ->seeInDatabase('enrollments', ['course_id' => 264, 'user_id'=>$user->id]);
