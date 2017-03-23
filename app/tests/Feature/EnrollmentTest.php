a611f73861aec189b3ccba9636b0cbf8d756678b
Nathan Chao <n2chao@gmail.com>
2017-03-23 11:06:15 -0400

    Change the course added in the test in order to see if it will work with Travis.

--- app/tests/Feature/EnrollmentTest.php
+++ app/tests/Feature/EnrollmentTest.php
@@ -24,9 +24,9 @@ class EnrollmentTest extends BrowserKitTestCase
         $user = \App\User::find(1);
         $this->actingAs($user)
             ->visit('courses/course')
-            ->type('soen331', 'add_course_ids[0]')
+            ->type('soen341', 'add_course_ids[0]')
             ->press('Add Courses')
-            ->seeInDatabase('enrollments', ['course_id' => 263, 'user_id'=>$user->id]);
+            ->seeInDatabase('enrollments', ['course_id' => 264, 'user_id'=>$user->id]);
 
     }
 
