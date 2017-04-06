97b12865eb09d137b6aee49d263cf23f99f5065c
patrickspensieri <patrick.spensieri@gmail.com>
2017-04-06 19:54:09 -0400

    #160 restructure course.js, selectize all newly created course input fields

--- app/public/js/course.js
+++ app/public/js/course.js
@@ -15,59 +15,121 @@ Which makes it more user-friendly now.*/
 
 //start selectize.js
 //inspired by https://maxoffsky.com/code-blog/laravel-shop-tutorial-3-implementing-smart-search/
+// jQuery(document).ready(function($){		//wrap jquery to prevent bug
+// 	$(document).ready(function(){
+// 	    // $('#searchbox').selectize({
+// 	    $('#course_name').selectize({
+// 			valueField: 'code',			//value when item is selected
+// 			labelField: 'code',			//not used if custom render function is defined
+// 			searchField: ['code'],		//what to analyze when filtering?
+// 			maxOptions: 10,
+// 			create: false,
+// 	        load: function(query, callback) {
+// 	            if (!query.length) return callback();
+// 	            $.ajax({
+// 	                url: '/search',		//see web.php
+// 	                type: 'GET',
+// 	                dataType: 'json',
+// 	                data: {
+// 	                    q: query
+// 	                },
+// 	                error: function(response) {
+// 	                    callback();
+// 	                },
+// 	                success: function(response) {
+// 	                    callback(response.data);
+// 	                }
+// 	            });
+// 	        },
+// 	    });
+// 	});
+// });
+//
 jQuery(document).ready(function($){		//wrap jquery to prevent bug
-	$(document).ready(function(){
-	    $('#searchbox').selectize({
+
+
+	selectizeRow = function(id){
+		// $('#course_name').selectize({
+		$('#' + id).selectize({
+
 			valueField: 'code',			//value when item is selected
 			labelField: 'code',			//not used if custom render function is defined
 			searchField: ['code'],		//what to analyze when filtering?
 			maxOptions: 10,
 			create: false,
-	        load: function(query, callback) {
-	            if (!query.length) return callback();
-	            $.ajax({
-	                url: '/search',		//see web.php
-	                type: 'GET',
-	                dataType: 'json',
-	                data: {
-	                    q: query
-	                },
-	                error: function(response) {
-	                    callback();
-	                },
-	                success: function(response) {
-	                    callback(response.data);
-	                }
-	            });
-	        },
-	    });
-	});
+			load: function(query, callback) {
+				if (!query.length) return callback();
+				$.ajax({
+					url: '/search',		//see web.php
+					type: 'GET',
+					dataType: 'json',
+					data: {
+						q: query
+					},
+					error: function(response) {
+						callback();
+					},
+					success: function(response) {
+						callback(response.data);
+					}
+				});
+			},
+		});
+	}
+
+	addRow = function(tableID){
+		var table = document.getElementById(tableID);
+		var rowCount = table.rows.length;
+		if(rowCount < 7){
+			var row = table.insertRow(rowCount);
+			var newcell = row.insertCell(0);
+			newcell.innerHTML = '<tr><p><td><div class="form-group"><label for="course_name_'+rowCount+'" class="col-md-4 control-label">Course Name: </label> <div class="col-md-6"> <select id="course_name_'+rowCount+'" type="text" placeholder="i.e SOEN341" class="form-control" name="add_course_ids['+rowCount+']"> </div> <input type="button" value="Remove" onClick="deleteRow(\'dataTable\')" class="btn btn-primary btn-xs"  /> </div> </td></p> </tr>'
+		}else{
+			alert("You can only add up to 7 courses.");
+
+		}
+		selectizeRow("course_name_"+rowCount);
+	}
+
+	deleteRow = function(tableID) {
+		var table = document.getElementById(tableID);
+		var rowCount = table.rows.length;
+		if(rowCount<=1){
+			alert("You must enter at least one course.")
+		}else{
+			document.getElementById(tableID).deleteRow(rowCount-1);
+			rowCount--;
+		}
+	}
+
+
+	$(document).ready(selectizeRow("course_name_0"));
 });
-//end selectize.js
 
-function addRow(tableID) {
 
-	var table = document.getElementById(tableID);
-	var rowCount = table.rows.length;
-	if(rowCount < 7){
-		var row = table.insertRow(rowCount);
-		var newcell = row.insertCell(0);
-		newcell.innerHTML = '<tr><p><td><div class="form-group"><label for="course_name" class="col-md-4 control-label">Course Name: </label> <div class="col-md-6"> <input id="course_name" type="text" placeholder="i.e SOEN341" class="form-control" name="add_course_ids['+rowCount+']"> </div> <input type="button" value="Remove" onClick="deleteRow(\'dataTable\')" class="btn btn-primary btn-xs"  /> </div> </td></p> </tr>'
-	}else{
-		alert("You can only add up to 7 courses.");
+// function addRow(tableID) {
+//
+// 	var table = document.getElementById(tableID);
+// 	var rowCount = table.rows.length;
+// 	if(rowCount < 7){
+// 		var row = table.insertRow(rowCount);
+// 		var newcell = row.insertCell(0);
+// 		newcell.innerHTML = '<tr><p><td><div class="form-group"><label for="course_name" class="col-md-4 control-label">Course Name: </label> <div class="col-md-6"> <input id="course_name" type="text" placeholder="i.e SOEN341" class="form-control" name="add_course_ids['+rowCount+']"> </div> <input type="button" value="Remove" onClick="deleteRow(\'dataTable\')" class="btn btn-primary btn-xs"  /> </div> </td></p> </tr>'
+// 	}else{
+// 		alert("You can only add up to 7 courses.");
+//
+// 	}
+// }
 
-	}
-}
-
-function deleteRow(tableID) {
-	var table = document.getElementById(tableID);
-	var rowCount = table.rows.length;
-	if(rowCount<=1){
-		alert("You must enter at least one course.")
-	}else{
-		document.getElementById(tableID).deleteRow(rowCount-1);
-		rowCount--;
-	}
+// function deleteRow(tableID) {
+// 	var table = document.getElementById(tableID);
+// 	var rowCount = table.rows.length;
+// 	if(rowCount<=1){
+// 		alert("You must enter at least one course.")
+// 	}else{
+// 		document.getElementById(tableID).deleteRow(rowCount-1);
+// 		rowCount--;
+// 	}
 	/*var table = document.getElementById(tableID);
 	var rowCount = table.rows.length;
 	for(var i=0; i<rowCount; i++) {
@@ -86,4 +148,4 @@ rowCount--;
 i--;
 }
 }*/
-}
+// }
--- app/resources/views/courses/course.blade.php
+++ app/resources/views/courses/course.blade.php
@@ -12,8 +12,8 @@
     <script type="text/javascript" src="/js/course.js"></script>
       <div class="col-md-9">
           <!-- test SELECT box -->
-          <meta name="csrf_token" content="{{ csrf_token() }}" />
-          <select id="searchbox" name="q" placeholder="Search..." class="form-control"></select>
+          <!-- <meta name="csrf_token" content="{{ csrf_token() }}" /> -->
+          <!-- <select id="searchbox" name="q" placeholder="Search..." class="form-control"></select> -->
 
         <div id="course-panel" class="panel panel-default">
           <div class="panel-heading">Current Classes</div>
@@ -56,9 +56,11 @@
                     <p>
                     <td>
                       <div class="form-group">
-                        <label for="course_name" class="col-md-4 control-label">Course Name: </label>
+                        <label for="course_name_0" class="col-md-4 control-label">Course Name: </label>
                         <div class="col-md-6">
-                          <input id="course_name" type="text" placeholder="i.e SOEN341" class="form-control" name="add_course_ids[0]" value="{{ old('course_name') }}">
+                            <select id="course_name_0" type="text" placeholder="i.e SOEN341" class="form-control" name="add_course_ids[0]" value="{{ old('course_name') }}">
+
+                          <!-- <input id="course_name" type="text" placeholder="i.e SOEN341" class="form-control" name="add_course_ids[0]" value="{{ old('course_name') }}"> -->
                         </div>
                         <input type="button" value="Remove" onClick="deleteRow('dataTable')" class="btn btn-primary btn-xs"  />
                       </div>
