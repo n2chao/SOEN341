b3cd765fe464a0fbf47132effb8338e155562671
patrickspensieri <patrick.spensieri@gmail.com>
2017-04-07 01:57:25 -0400

    #160 small UI edit, clean out comments

--- app/public/js/course.js
+++ app/public/js/course.js
@@ -13,43 +13,9 @@
 however, the deleteRow() I changed it so that users won't have to check the corresponded boxes whenever the user removes the courses.
 Which makes it more user-friendly now.*/
 
-//start selectize.js
 //inspired by https://maxoffsky.com/code-blog/laravel-shop-tutorial-3-implementing-smart-search/
-// jQuery(document).ready(function($){		//wrap jquery to prevent bug
-// 	$(document).ready(function(){
-// 	    // $('#searchbox').selectize({
-// 	    $('#course_name').selectize({
-// 			valueField: 'code',			//value when item is selected
-// 			labelField: 'code',			//not used if custom render function is defined
-// 			searchField: ['code'],		//what to analyze when filtering?
-// 			maxOptions: 10,
-// 			create: false,
-// 	        load: function(query, callback) {
-// 	            if (!query.length) return callback();
-// 	            $.ajax({
-// 	                url: '/search',		//see web.php
-// 	                type: 'GET',
-// 	                dataType: 'json',
-// 	                data: {
-// 	                    q: query
-// 	                },
-// 	                error: function(response) {
-// 	                    callback();
-// 	                },
-// 	                success: function(response) {
-// 	                    callback(response.data);
-// 	                }
-// 	            });
-// 	        },
-// 	    });
-// 	});
-// });
-//
 jQuery(document).ready(function($){		//wrap jquery to prevent bug
-
-
 	selectizeRow = function(id){
-		// $('#course_name').selectize({
 		$('#' + id).selectize({
 
 			valueField: 'code',			//value when item is selected
@@ -83,10 +49,9 @@ jQuery(document).ready(function($){		//wrap jquery to prevent bug
 		if(rowCount < 7){
 			var row = table.insertRow(rowCount);
 			var newcell = row.insertCell(0);
-			newcell.innerHTML = '<tr><p><td><div class="form-group"><label for="course_name_'+rowCount+'" class="col-md-4 control-label">Course Name: </label> <div class="col-md-6"> <select id="course_name_'+rowCount+'" type="text" placeholder="i.e SOEN341" class="form-control" name="add_course_ids['+rowCount+']"> </div> <input type="button" value="Remove" onClick="deleteRow(\'dataTable\')" class="btn btn-primary btn-xs"  /> </div> </td></p> </tr>'
+			newcell.innerHTML = '<tr><td><div class="col-md-6"> <select id="course_name_'+rowCount+'" type="text" placeholder="i.e SOEN341" class="form-control" name="add_course_ids['+rowCount+']"> </div> <input type="button" value="Remove" onClick="deleteRow(\'dataTable\')" class="btn btn-primary btn-xs"  /></td></tr>'
 		}else{
 			alert("You can only add up to 7 courses.");
-
 		}
 		selectizeRow("course_name_"+rowCount);
 	}
@@ -101,51 +66,6 @@ jQuery(document).ready(function($){		//wrap jquery to prevent bug
 			rowCount--;
 		}
 	}
-
-
+	//selectize first input when document ready
 	$(document).ready(selectizeRow("course_name_0"));
 });
-
-
-// function addRow(tableID) {
-//
-// 	var table = document.getElementById(tableID);
-// 	var rowCount = table.rows.length;
-// 	if(rowCount < 7){
-// 		var row = table.insertRow(rowCount);
-// 		var newcell = row.insertCell(0);
-// 		newcell.innerHTML = '<tr><p><td><div class="form-group"><label for="course_name" class="col-md-4 control-label">Course Name: </label> <div class="col-md-6"> <input id="course_name" type="text" placeholder="i.e SOEN341" class="form-control" name="add_course_ids['+rowCount+']"> </div> <input type="button" value="Remove" onClick="deleteRow(\'dataTable\')" class="btn btn-primary btn-xs"  /> </div> </td></p> </tr>'
-// 	}else{
-// 		alert("You can only add up to 7 courses.");
-//
-// 	}
-// }
-
-// function deleteRow(tableID) {
-// 	var table = document.getElementById(tableID);
-// 	var rowCount = table.rows.length;
-// 	if(rowCount<=1){
-// 		alert("You must enter at least one course.")
-// 	}else{
-// 		document.getElementById(tableID).deleteRow(rowCount-1);
-// 		rowCount--;
-// 	}
-	/*var table = document.getElementById(tableID);
-	var rowCount = table.rows.length;
-	for(var i=0; i<rowCount; i++) {
-	var row = table.rows[i];
-
-	var chkbox = row.cells[0].childNodes[0];
-
-
-	if(null != chkbox && true == chkbox.checked) {
-	if(rowCount <= 1) { 						// limit the user from removing all the fields
-	alert("Must enter one course at least.");
-	break;
-}
-table.deleteRow(i);
-rowCount--;
-i--;
-}
-}*/
-// }
--- app/resources/views/courses/course.blade.php
+++ app/resources/views/courses/course.blade.php
@@ -11,10 +11,6 @@
     <script type="text/javascript" src='{{ url("/js/selectize/js/standalone/selectize.min.js") }}'></script>
     <script type="text/javascript" src="/js/course.js"></script>
       <div class="col-md-9">
-          <!-- test SELECT box -->
-          <!-- <meta name="csrf_token" content="{{ csrf_token() }}" /> -->
-          <!-- <select id="searchbox" name="q" placeholder="Search..." class="form-control"></select> -->
-
         <div id="course-panel" class="panel panel-default">
           <div class="panel-heading">Current Classes</div>
           <div class="panel-body">
@@ -50,22 +46,15 @@
               </p>
               {{csrf_field()}}
               <fieldset class="row2">
-                <table id="dataTable" class="form" border="0px">
+                <table id="dataTable" class="table table-hover">
                   <tbody>
                   <tr>
-                    <p>
                     <td>
-                      <div class="form-group">
-                        <label for="course_name_0" class="col-md-4 control-label">Course Name: </label>
                         <div class="col-md-6">
                             <select id="course_name_0" type="text" placeholder="i.e SOEN341" class="form-control" name="add_course_ids[0]" value="{{ old('course_name') }}">
-
-                          <!-- <input id="course_name" type="text" placeholder="i.e SOEN341" class="form-control" name="add_course_ids[0]" value="{{ old('course_name') }}"> -->
                         </div>
                         <input type="button" value="Remove" onClick="deleteRow('dataTable')" class="btn btn-primary btn-xs"  />
-                      </div>
                     </td>
-                    </p>
                   </tr>
                   </tbody>
                 </table>
