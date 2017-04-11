9f9e8ec9af83dbe0e16707bde16de461015abf15
alimouawieh <alimouawieh@gmail.com>
2017-04-10 22:45:02 -0400

    Update common.blade.php

--- app/resources/views/common.blade.php
+++ app/resources/views/common.blade.php
@@ -2,67 +2,50 @@
 
 <div class="container">
   <div class="row">
-    <!-- Left column -->
-    <div class="col-md-3">
+   <div class="col-md-3">
 
-      <!-- DASH SIDEBAR -->
-      <div id="dash-sidebar" class="dash">
+      <!-- SIDEBAR -->
+     <div>
 
-         <!-- SIDEBAR USER TITLE -->
-         <div>
-             <h3>{{ Auth::user()->name }}</h3>
-         </div>
-         <!-- END SIDEBAR USER TITLE -->
+       <!-- SIDEBAR USER TITLE -->
+       <div>
+           <h3>{{ Auth::user()->name }}</h3>
+       </div>
+       <!-- END SIDEBAR USER TITLE -->
 
-         <!-- SIDEBAR MENU -->
-         <div>
-           <ul class="nav">
-             <li class="active default">
-               <a href="/home">
-               Overview </a>
-             </li>
-             <li>
-               <a href="#">
-               Account Settings </a>
-             </li>
-             <li>
-               <a href="/requests">
-               Find a Buddy </a>
-             </li>
-             <li>
-               <a href="/instructors/chooseinstr">
-                Find an Instructor</a>
-             </li>
-             <li>
-               <a href="/schedule">
-                Edit Availabilities</a>
-             </li>
-             <li>
-               <a href="/courses/course">
-                Edit Courses</a>
-             </li>
-           </ul>
-         </div>
-         <!-- END MENU -->
-      </div>
-    </div>
-    <!-- End Left column -->
-
-    <!-- Main Right Column -->
-    <div class="col-md-9">
-
-    <!-- GLOBAL ANNOUNCEMENT -->
-    @component('global-announcement')
-    @endcomponent
-    <!-- END GLOBAL ANNOUNCEMENT -->
-
-
-     <!-- Main Right Content -->
-     @yield('extra-content')
-     <!-- Main Right Content -->
+       <!-- SIDEBAR MENU -->
+       <div>
+         <ul class="nav">
+           <li class="active default">
+             <a href="/home">
+             Overview </a>
+           </li>
+           <li>
+             <a href="#">
+             Account Settings </a>
+           </li>
+           <li>
+             <a href="/requests">
+             Find a Buddy </a>
+           </li>
+           <li>
+             <a href="/instructors/chooseinstr">
+              Find an Instructor</a>
+           </li>
+           <li>
+             <a href="/schedule">
+              Edit Availabilities</a>
+           </li>
+           <li>
+             <a href="/courses/course">
+              Edit Courses</a>
+           </li>
+         </ul>
+       </div>
+       <!-- END MENU -->
+     </div>
    </div>
-   <!-- End Main content -->
-
-  </div>
+  @yield('extra-content')
+</div>
 </div>
 @endsection
