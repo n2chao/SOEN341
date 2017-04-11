df9ce76fade5c450c190b9af86b14fb832ce3c57
alimouawieh <alimouawieh@gmail.com>
2017-04-10 22:52:44 -0400

    Add files via upload

---
+++ app/instructors/chooseinstr.blade.php
@@ -0,0 +1,59 @@
+@extends('layouts.app')
+
+@section('extra-content')
+	<div class="col-md-9">
+        <div class="profile-content">
+          	<div class="col-md-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">Choose an Instructor to Meet Wtih</div>
+					<div class="panel-body">
+
+						<!-- <form id="instructor-names" method="POST" action="/instructorMeeting"> -->
+						<form id="instructor-names" method="GET" action="/choosetime">
+
+							<div class='col-md-6'>
+								<div class="radio">
+
+									<h4>Teachers</h4>
+									@foreach ($teachers as $teacher_name => $teacher_id)
+										<label><input id="teacher-name" name="instructor" value="{{$teacher_id}}" type="radio">{{ $teacher_name }}</label></br>
+									@endforeach
+
+								</div>
+							</div>
+
+							<div class='col-md-6'>
+								<div class='radio'>
+									<h4>Teaching Assistants</h4>
+									@foreach ($tas as $ta_name => $ta_id)
+										<label><input id="ta-name" name="instructor" value="{{$ta_id}}" type="radio">{{ $ta_name }}</label></br>
+									@endforeach
+								</div>
+							</div>
+
+							<div class='row'>
+								<div class='col-md-3'>
+									<button class="btn btn-default" type="submit" value="selection" name="instructor-names-next">Next</button>
+								</div>
+							</div>
+
+							@if (count($errors))
+								<div class='form-group'>
+									<div class='alert alert-danger'>
+										<ul>
+											@foreach ($errors->all() as $error)
+												<li>{{ $error }}</li>
+											@endforeach
+										</ul>
+									</div>
+								</div>
+							@endif
+
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
+@include('common')
---
+++ app/instructors/choosetime.blade.php
@@ -0,0 +1,184 @@
+@extends('layouts.app')
+
+@section('extra-content')
+	<div class="col-md-9">
+        <div class="profile-content">
+          	<div class="col-md-12">
+				<div class="panel panel-default">
+					<div class="panel-heading">Choose a Meeting Time</div>
+					<div class="panel-body">
+
+						<!-- Form for changing the week -->
+						<form id="changeWeek" method="GET" action="/choosetime">
+							{{csrf_field()}}
+							<input type="hidden" name="instructor" value="{{$instructor->id}}">
+							<input type="hidden" name="weekStart" value="{{$week[0]}}">
+							<input type="hidden" name="weekEnd" value="{{$week[1]}}">
+							
+							<div class='row'>
+								<div class='col-md-8'>
+									<div class='container' style='margin: 5px;'>
+										{{$instructor->name}}
+									</div>
+								</div>
+								<div class='col-md-4'>
+									<div class='container' style='margin: 5px;'>
+										<button type="btn btn-default" name="prevWeek" value="prevWeek">Previous Week</button>
+										<button type="btn btn-default" name="nextWeek" value="nextWeek">Next Week</button>
+									</div>
+								</div>
+							</div>	
+							
+
+						</form>
+							
+							<div class="row">
+								<div class="col-md-12">
+									<form id="instructor-times" method="POST" action="/instructorMeeting">
+										{{csrf_field()}}
+										<input type="hidden" name="currentWeek" value="{{$week[0]}}">
+										<input type="hidden" name="instructor" value="{{$instructor->id}}">
+										<?php 
+											date_default_timezone_set("America/New_York");
+											echo "<table class='table .table-bordered'><thread><tr>";
+													
+												$i = $week[0];
+												if(date("l", $i) != "Sunday"){
+													while(date("l", $i) != "Sunday"){
+														$i = strtotime("-1 day", $i);
+													}
+												}
+												
+												$display = date("D, M j", $i);
+												echo "<th> $display </th>";
+
+												$i = strtotime("+1 day", $i);
+												$display = date("D, M j", $i);
+												echo "<th> $display </th>";
+
+												$i = strtotime("+1 day", $i);
+												$display = date("D, M j", $i);
+												echo "<th> $display </th>";
+
+												$i = strtotime("+1 day", $i);
+												$display = date("D, M j", $i);
+												echo "<th> $display </th>";
+
+												$i = strtotime("+1 day", $i);
+												$display = date("D, M j", $i);
+												echo "<th> $display </th>";
+
+												$i = strtotime("+1 day", $i);
+												$display = date("D, M j", $i);
+												echo "<th> $display </th>";
+
+												$i = strtotime("+1 day", $i);
+												$display = date("D, M j", $i);
+												echo "<th> $display </th>";
+										
+											echo "</tr></thread> <tbody> <tr> <td>";
+												
+												$day = "/^Sunday/s";
+
+												echo "<div class='col-sm-12'><div class='radio'>";
+												foreach ($finalMatch as $match){
+													if(preg_match($day, $match)){
+														$time = substr($match, 6);
+														echo "<input type='radio' name='start_time' value='$match'>$time</br>";
+													}
+												}
+												echo "</div></div>";
+
+											echo "</td><td>";
+
+												$day = "/^Monday/s";
+												echo "<div class='col-sm-12'><div class='radio'>";
+												foreach ($finalMatch as $match){
+													if(preg_match($day, $match)){
+														$time = substr($match, 6);
+														echo "<input type='radio' name='start_time' value='$match'>$time</br>";
+													}
+												}
+												echo "</div></div>";
+
+											echo "</td><td>";
+											$day = "/^Tuesday/s";
+												echo "<div class='col-sm-12'><div class='radio'>";
+												foreach ($finalMatch as $match){
+													if(preg_match($day, $match)){
+														$time = substr($match, 7);
+														echo "<input type='radio' name='start_time' value='$match'>$time</br>";
+													}
+												}
+												echo "</div></div>";
+
+											echo "</td><td>";
+											$day = "/^Wednesday/s";
+												echo "<div class='col-sm-12'><div class='radio'>";
+												foreach ($finalMatch as $match){
+													if(preg_match($day, $match)){
+														$time = substr($match, 9);
+														echo "<input type='radio' name='start_time' value='$match'>$time</br>";
+													}
+												}
+												echo "</div></div>";
+
+											echo "</td><td>";
+											$day = "/^Thursday/s";
+												echo "<div class='col-sm-12'><div class='radio'>";
+												foreach ($finalMatch as $match){
+													if(preg_match($day, $match)){
+														$time = substr($match, 8);
+														echo "<input type='radio' name='start_time' value='$match'>$time</br>";
+													}
+												}
+												echo "</div></div>";
+
+											echo "</td><td>";
+											$day = "/^Friday/s";
+												echo "<div class='col-sm-12'><div class='radio'>";
+												foreach ($finalMatch as $match){
+													if(preg_match($day, $match)){
+														$time = substr($match, 6);
+														echo "<input type='radio' name='start_time' value='$match'>$time</br>";
+													}
+												}
+												echo "</div></div>";
+
+											echo "</td><td>";
+											$day = "/^Saturday/s";
+												echo "<div class='col-sm-12'><div class='radio'>";
+												foreach ($finalMatch as $match){
+													if(preg_match($day, $match)){
+														$time = substr($match, 8);
+														echo "<input type='radio' name='start_time' value='$match'>$time</br>";
+													}
+												}
+												echo "</div></div>";
+											
+											echo "</td></tr> </tbody></table>";
+										?>
+										<button class="btn btn-default" type="submit" name="instructor-names-next">Add to Schedule</button>
+										@if (count($errors))
+											<div class='form-group'>
+												<div class='alert alert-danger'>
+													<ul>
+														@foreach ($errors->all() as $error)
+															<li>{{ $error }}</li>
+														@endforeach
+													</ul>
+												</div>
+											</div>
+										@endif
+									</form>
+								</div>
+							</div>
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection
+@include('common')
