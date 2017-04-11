f2345d0cbf2e95208f232e40dc51916cd7b51b0a
alimouawieh <alimouawieh@gmail.com>
2017-04-10 22:46:24 -0400

    Add files via upload

---
+++ app/resources/views/information/aboutus.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="col-md-12">
+        <div class="profile-content">
+				<div class="panel panel-default">
+					<div class="panel-heading">About us</div>
+					<div class="panel-body">
+
+						<p>test</p>
+					</div>
+				</div>
+			</div>
+	</div>
+@endsection
---
+++ app/resources/views/information/info.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="col-md-12">
+        <div class="profile-content">
+
+				<div class="panel panel-default">
+					<div class="panel-heading">More Information about Study Buddy!</div>
+					<div class="panel-body">
+
+
+							<div>
+									<h4>Student</h4>
+								<h4>		As a Student you can register on our Website to find new friends that are taking <thead>
+									taking similar Courses like you and would like to study in a group.
+								</thead>
+									</h4>
+									<h4> Want Higher grades ? </h4>
+									<h4> Register on our Website and never fall behind in any course anymore! </h4>
+							</div>
+
+							<div>
+									<h4>Teacher</h4>
+									<h4> As a Teacher you can put your available times so students who would
+										like to request a meeting with you can easily be done according to your schedule
+									</h4>
+							</div>
+
+					</div>
+				</div>
+		</div>
+	</div>
+@endsection
