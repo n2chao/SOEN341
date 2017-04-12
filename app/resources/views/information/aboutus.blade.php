8a22c4117de6f2a3d46c5b40dd5c12450d075f6e
alimouawieh <Ali@alis-macbook-pro.local>
2017-04-12 00:53:31 -0400

    Added "About us" page #174
    Added about us page in footer on the website. User can now access about
    us to see more information about developing team

---
+++ app/resources/views/information/aboutus.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+	<div class="col-md-12">
+        <div class="profile-content">
+				<div class="panel panel-default">
+					<div class="panel-heading">About Us</div>
+					<br>
+					<p align="left">
+						<font size="5">
+						<center>
+							We are a fiercely motivated young team dedicated
+						<br>
+						to improve and help modernize student's
+							<br>
+					 learning experience. Our website will allow
+							<br>
+			 students to connect with others by studying
+					 	<br>
+				 together depending on their classes. Students
+							<br>
+				will also be able to contact instructors. Our
+							<br>
+
+
+						web-based environment is secure and allows
+							<br>
+						  the delivery of course activities and ressources.
+						 	<br>
+
+
+					</font></p>
+					<br>
+<center>
+					<body>
+						<img src="http://www.itstaff.com/wp-content/uploads/2015/08/Software-Engineers-and-Developers.jpg" height="450" width="600">
+					<div class="panel-body">
+</body>
+<hr>
+						<p> <i>Support team - Call us at 514-848-2424 </i></p>
+					<p><i>email: concordia@gmail.com </i> </p>
+					</div>
+
+
+				</div>
+			</div>
+	</div>
+@endsection
