d2834cb6d187a8a52512e6aac4aad18d06e4f55d
alimouawieh <alimouawieh@gmail.com>
2017-04-11 22:53:11 -0400

    Update common.blade.php

--- app/resources/views/common.blade.php
+++ app/resources/views/common.blade.php
@@ -5,7 +5,58 @@
     <!-- Left column -->
     <div class="col-md-3">
 
-      <!-- DASH SIDEBAR -->
+      <!@section('content')
+
+<div class="container">
+  <div class="row">
+   <div class="col-md-3">
+
+      <!-- SIDEBAR -->
+     <div>
+
+       <!-- SIDEBAR USER TITLE -->
+       <div>
+           <h3>{{ Auth::user()->name }}</h3>
+       </div>
+       <!-- END SIDEBAR USER TITLE -->
+
+       <!-- SIDEBAR MENU -->
+       <div>
+         <ul class="nav">
+           <li class="active default">
+             <a href="/home">
+             Overview </a>
+           </li>
+           <li>
+             <a href="#">
+             Account Settings </a>
+           </li>
+           <li>
+             <a href="/requests">
+             Find a Buddy </a>
+           </li>
+           <li>
+             <a href="/instructors/chooseinstr">
+              Find an Instructor</a>
+           </li>
+           <li>
+             <a href="/schedule">
+              Edit Availabilities</a>
+           </li>
+           <li>
+             <a href="/courses/course">
+              Edit Courses</a>
+           </li>
+         </ul>
+       </div>
+       <!-- END MENU -->
+     </div>
+   </div>
+  @yield('extra-content')
+</div>
+</div>
+@endsection
+-- DASH SIDEBAR -->
       <div id="dash-sidebar" class="dash">
 
          <!-- SIDEBAR USER TITLE -->
