7e9aa1a9a1a3e4000baac3dd989f5f0215159dba
alimouawieh <alimouawieh@gmail.com>
2017-04-11 22:56:29 -0400

    Update app.blade.php

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -12,7 +12,128 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Styles<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Custom CSS -->
+    <link media="all" type="text/css" rel="stylesheet" href="{{asset('css/style.css')}}">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
     <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+</head>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                     <!-- Branding Image -->
+                    <a class="navbar-brand"
+                    @if (Auth::guest())
+                        href="{{ url('/') }}"
+                    @else
+                        href="{{ url('/home') }}"
+                    @endif >
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                        <li><a href="{{ url('/information/info') }}"> Info</a> </li>
+                    </ul>
+
+
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                            <li><a href="{{ url('/login') }}">Login</a></li>
+                            <li><a href="{{ url('/register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                  <li>
+                                        <a href="{{ url('/home') }}">
+                                          Dashboard
+                                        </a>
+                                  </li>
+                                    <li>
+                                        <a href="{{ url('/logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <section class="wrap-content">
+        	@yield('content')
+        </section>
+
+
+        <!--Footer-->
+        <footer>
+          <div class="col-md-12">
+            <div class="pull-left">
+              <a href="{{url('/information/aboutus')}}">About Us</a>
+            </div>
+            <div class="pull-center">
+              <h5>THANKS FOR VISITING!</h5>
+            </div>
+          </div>
+
+        </footer>
+    </div>
+
+
+    <!-- Scripts -->
+    <script src="/js/app.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+</body>
+</html>
+ -->
     <link href="/css/app.css" rel="stylesheet">
 
     <!-- Scripts -->
