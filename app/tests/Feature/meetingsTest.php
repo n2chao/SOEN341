7fc76c890d3e1794de65662d9361ccc16d59fb7e
patrickspensieri <patrick.spensieri@gmail.com>
2017-04-17 15:55:51 -0400

    #133 fix createMeetingSuccess test

--- app/tests/Feature/meetingsTest.php
+++ app/tests/Feature/meetingsTest.php
@@ -17,44 +17,24 @@ class meetingsTest extends TestCase
     //use WithoutMiddleware;
     use DatabaseTransactions;
 
+    /**
+    * Test meeting creation success.
+    * NOTE : Test is highly dependent on "db:seed" command, should be refactored.
+    */
     public function testCreateMeetingSuccess(){
-        // $this->artisan("migrate:refresh");
-        // $this->artisan("db:seed");
-        // $course = \App\Course::find(1);
-        //get student and teacher enrolled in course
-        // $student = $course->users->where('title', '=', 'student')->values()->get(0);
-        // $teacher = $course->users->where('title', '=', 'teacher')->values()->get(0);
-        // Auth::login($student);
-        //
-        factory(User::class)->create([
-            'name' => 'bob',
-            'title' => 'student',
-            'email' => 'bob@bob.com',
-        ]);
-        factory(User::class)->create([
-            'name' => 'tom',
-            'title' => 'teacher',
-            'email' => 'tom@tom.com',
-        ]);
-        factory(Course::class)->create([
-            'code' => 'SOEN341',
-            'name' => 'Software Processes',
-        ]);
-        factory(Enrollment::class)->create([
-            'course_id' => '1',
-            'user_id' => '1',
-        ]);
-        factory(Enrollment::class)->create([
-            'course_id' => '1',
-            'user_id' => '2',
-        ]);
-        $student = User::where('name','=','bob');
-        $teacher = User::where('name','=','tom');
+        $this->artisan("db:seed");
+        $course = \App\Course::find(1);
+        if($course->users->count() < 2){
+          $course = \App\Course::find(2);
+        }
+        $student = $course->users->get(0);
+        $student->title = ('student');
+        $teacher = $course->users->get(1);
+        $teacher->title = ('teacher');
+        Auth::login($student);
         //seeded schedules are random, assign fixed values
         $student->schedule->freetime = "111011100010010011001100011111101001111100100010100011010000010110010110001011111010001010010100000001110101111100011011000000111011001010000110100101001001001001101010";
         $teacher->schedule->freetime = "101011100010010011001100011111101001111100100010100011010000010110010110001011111010001010010100000001110101111100011011000000111011001010000110100101001001001001101010";
-        // fwrite(STDERR, print_r($student->schedule->freetime, TRUE));
-        // fwrite(STDERR, print_r($teacher->schedule->freetime, TRUE));
         // fwrite(STDERR, print_r(Auth::user()->schedule->freetime, TRUE));
         //meeting time will occur in the past?
         $response = $this->call('POST', '/instructorMeeting', [
@@ -70,17 +50,13 @@ class meetingsTest extends TestCase
             'id' => '1',
             'instructorMeeting' => true
         ]);
-        // $this->seeInDatabase('attendances', [
-        //     'meeting_id' => '1',
-        //     'user_id' => $student->id
-        // ]);
-        // $this->seeInDatabase('attendances', [
-        //     'meeting_id' => '1',
-        //     'user_id' => $teacher->id
-        // ]);
-    }
-
-    // public function testDeclineMeetingSuccess(){
-    //
-    // }
+        $this->assertDatabaseHas('attendances', [
+            'meeting_id' => '1',
+            'user_id' => $student->id
+        ]);
+        $this->assertDatabaseHas('attendances', [
+            'meeting_id' => '1',
+            'user_id' => $teacher->id
+        ]);
+      }
 }
